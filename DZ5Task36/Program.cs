f5f99ecf84f8cfb09dd6a7e5af83c54a39582434
Dima_Altergot <alter.001@yandex.ru>
2023-03-16 22:09:54 +0300

    дз5

---
+++ DZ5Task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int a = 0; a < numbers.Length; a+=2)               //Считаем сумму
+    sum = sum + numbers[a];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)              //Создаем масив
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)                  //Пищем масив
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ DZ5Тask34/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine(" Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int a = 0; a < numbers.Length; a++)                //ищем колличеством четных
+if (numbers[a] % 2 == 0)
+count++;
+
+Console.WriteLine($"Четных {count}");
+
+void FillArrayRandomNumbers(int[] numbers)                     //Создаем масив
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)                              //Пишем масив
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
